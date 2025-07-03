83961d9641228643fda0bfb82a47066545774f64
Julian König <julian.koenig@js-soft.com>
2025-07-03 11:09:46 +0200

    fix: access ferretdb unauthorized

--- start-ferretdb.sh
+++ start-ferretdb.sh
@@ -11,13 +11,14 @@ docker network create ferretdb
 docker run --network ferretdb --name postgres \
   -e POSTGRES_USER=username \
   -e POSTGRES_PASSWORD=password \
-  -e POSTGRES_DB=ferretdb \
+  -e POSTGRES_DB=postgres \
   -d ghcr.io/ferretdb/postgres-documentdb:latest
 
 docker run --network ferretdb --name ferretdb \
   -p $FERRETDB_PORT:27017 \
-  -e FERRETDB_POSTGRESQL_URL=postgres://username:password@postgres:5432/ferretdb?pool_max_conns=40 \
+  -e FERRETDB_POSTGRESQL_URL=postgres://username:password@postgres:5432/postgres?pool_max_conns=40 \
   -e FERRETDB_TELEMETRY=$FERRETDB_TELEMETRY \
+  -e FERRETDB_AUTH=false \
   -d ghcr.io/ferretdb/ferretdb:$FERRETDB_VERSION
 
 if [ $? -ne 0 ]; then
