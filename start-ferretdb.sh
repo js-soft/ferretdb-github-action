8346a881c69015a0c8b3f769a6210f60af138acf
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-07-03 11:10:53 +0200

    Access ferretdb unauthorized (#9)
    * chore: remove old param
    
    * fix: access ferretdb unauthorized
    
    * chore: actually test acessing the db

--- start-ferretdb.sh
+++ start-ferretdb.sh
@@ -3,7 +3,6 @@
 FERRETDB_VERSION=$1
 FERRETDB_PORT=$2
 FERRETDB_TELEMETRY=$3
-USE_POSTGRES=$4
 
 echo "Starting FerretDB version ${FERRETDB_VERSION} on port ${FERRETDB_PORT}"
 
@@ -12,13 +11,14 @@ docker network create ferretdb
 docker run --network ferretdb --name postgres \
   -e POSTGRES_USER=username \
   -e POSTGRES_PASSWORD=password \
-  -e POSTGRES_DB=ferretdb \
+  -e POSTGRES_DB=postgres \
   -d ghcr.io/ferretdb/postgres-documentdb:latest
 
 docker run --network ferretdb --name ferretdb \
   -p $FERRETDB_PORT:27017 \
-  -e FERRETDB_POSTGRESQL_URL=postgres://username:password@postgres:5432/ferretdb?pool_max_conns=40 \
+  -e FERRETDB_POSTGRESQL_URL=postgres://username:password@postgres:5432/postgres?pool_max_conns=40 \
   -e FERRETDB_TELEMETRY=$FERRETDB_TELEMETRY \
+  -e FERRETDB_AUTH=false \
   -d ghcr.io/ferretdb/ferretdb:$FERRETDB_VERSION
 
 if [ $? -ne 0 ]; then
